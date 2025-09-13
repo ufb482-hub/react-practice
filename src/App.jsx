9647dcef7c1ef5c0fb10ee67b87a35e589c7967a
ufb482-hub <ufb482@gmail.com>
2025-09-13 09:06:11 +0500

    some input field is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState ,useRef} from 'react'
 import { StudentData } from './components/StudentData';
 const App = () => {
   const [count,setcount]=useState(0);
+  const [name, setName] = useState("")
   let a=useRef(0);
   const btnref=useRef();
  useEffect( () => {
@@ -13,15 +14,23 @@ const App = () => {
 useEffect(() => {
   alert("i am running on just onely first rendering after this you not see me untill refresh page")
 }, [])
-useEffect(() => {
- alert("i am just running on every rendring")
-})
-
+// useEffect(() => {
+//  alert("i am  running on every rendring")
+// })
+const handleName=(e)=>{
+  setName(e.target.value);
+}
   return (
   <>
   <StudentData/>
   <button ref={btnref}  onClick={()=>{setcount(count+1)}}>The count is {count}</button>
   <button  onClick={()=>{btnref.current.style.display="none"}} onDoubleClick={()=>{btnref.current.style.display="block"}}>Remove counter button display</button>
+ 
+<p>Enter your Name</p>
+
+ <input type="text" value={name} onChange={handleName} />
+
+ <p>Name is : {name}</p>
   </>
   )
 }
