bf7fd5b18b70a7a661bc1df1acef241e3763f9eb
ufb482-hub <ufb482@gmail.com>
2025-09-13 08:40:27 +0500

    making desing

--- src/components/StudentData.jsx
+++ src/components/StudentData.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import studentdata from '../api/studentdata.json'
+import './StudentData.css'
  export const StudentData = () => {
     return (
         <>
-        <ol>
+        <ol className='order-list'>
             {studentdata.map((student,index)=>{
                 return(
-                <li key={index}>
-                     <p>ID :{student.id}</p>
-                    <p>Name :{student.name}</p>
-                    <p>Age :{student.age}</p>
+                <li key={index} className='lists'>
+                     <p className='id'>ID :{student.id}</p>
+                    <p className='name'>Name :{student.name}</p>
+                    <p className='age'>Age :{student.age}</p>
                 </li>)
             })}
         </ol>
